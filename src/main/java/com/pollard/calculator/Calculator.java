685607dc9388247eb6fecd60bc5af77bb02ecaef
Jason Pollard <jpollard91@gmail.com>
2018-11-13 21:46:43 -0500

    Added rest service and basic tests

--- src/main/java/com/pollard/calculator/Calculator.java
+++ src/main/java/com/pollard/calculator/Calculator.java
@@ -1,4 +1,10 @@
 package com.pollard.calculator;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class Calculator {
+    int sum(int a, int b) {
+        return a + b;
+    }
 }
